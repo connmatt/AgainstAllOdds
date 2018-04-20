2420cfee3f4916256c3c3fc15f18689499c23140
Matthew Connolly <conn6273@csumb.ad.csumb.edu>
2018-04-19 22:12:56 -0700

    phone with man....next step get the pause button to work

---
+++ Against-All-Odds/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    GameObject phone;
+    Component pauseCam;
+	// Use this for initialization
+	void Start () {
+        pauseCam = phone.GetComponent("Camera");
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("C"))
+        {
+           
+        }
+	}
+}
