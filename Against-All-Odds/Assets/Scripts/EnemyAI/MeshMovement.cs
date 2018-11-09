11859c30ae7f81ef2a9177b3494bea7239ce8f40
Brandon Mendel <mend7574@csumb.ad.csumb.edu>
2018-11-09 15:42:43 -0800

    Messing around with nav meshes, started test file

---
+++ Against-All-Odds/Assets/Scripts/EnemyAI/MeshMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeshMovement : MonoBehaviour {
+
+    public Transform target;
+    // Use this for initialization
+    protected virtual void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+	}
+
+}
---
+++ Against-All-Odds/Assets/Scripts/EnemyAI/WendigoMeshMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WendigoMeshMovement : MeshMovement {
+
+    public bool soundTriggered;
+    public bool stopping;
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+        soundTriggered = false;
+        stopping = false;
+	}
+	
+	// Update is called once per frame
+    /*
+	void Update () {
+        if (!stopping)
+        {
+            base.Update();
+        }
+        if ((!soundTriggered) && (player.GetComponent<PlayerController>().crouching == false) && (Vector3.Distance(transform.position, player.transform.position) <= 7f))
+        {
+            soundTriggered = true;
+        }
+    }
+    */
+}
